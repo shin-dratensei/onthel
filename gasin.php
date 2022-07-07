b5d4f69ed8e7ad61e2897ab95fffbc3ae536cb2f
shin-dratensei <107156505+shin-dratensei@users.noreply.github.com>
2022-07-07 08:00:53 +0700

    Update gasin.php

--- gasin.php
+++ gasin.php
@@ -34,167 +34,85 @@ function change(){
         echo color("green","===========(NGEREDEEM VOUCHER)===========");
         echo "\n".color("yellow","!] Claim voc GORIDE ");
         echo "\n".color("yellow","!] sek entenono");
-        for($a=1;$a<=3;$a++){
-        echo color("yellow",".");
+        echo "\e[!] Trying to redeem Voucher : GOFOODSANTAI19 !\n";
         sleep(3);
+        $claim = claim($verif);
+        if ($claim == false)
+            {
+            echo "\e[!]".$claim."\n";
+            sleep(3);
+            echo "\e[!] Trying to redeem Voucher : GOFOODSANTAI11 !\n";
+            sleep(3);
+            goto next;
+            }
+            else{
+                echo "\e[+] ".$claim."\n";
+                sleep(3);
+                echo "\e[!] Trying to redeem Voucher : COBAINGOJEK !\n";
+                sleep(3);
+                goto ride;
+            }
+            next:
+            $claim = claim1($verif);
+            if ($claim == false) {
+                echo "\e[!]".$claim['data']['message']."\n";
+                sleep(3);
+                echo "\e[!] Trying to redeem Voucher : GOFOODSANTAI08 !\n";
+                sleep(3);
+                goto next1;
+            }
+            else{
+                echo "\e[+] ".$claim."\n";
+                sleep(3);
+                echo "\e[!] Trying to redeem Voucher : COBAINGOJEK !\n";
+                sleep(3);
+                goto ride;
+            }
+            next1:
+            $claim = claim2($verif);
+            if ($claim == false) {
+                echo "\e[!]".$claim['errors'][0]['message']."\n";
+                sleep(3);
+                echo "\e[!] Trying to redeem Voucher : COBAINGOJEK !\n";
+                sleep(3);
+                goto ride;
+            }
+          else
+            {
+            echo "\e[+] ".$claim . "\n";
+            sleep(3);
+            echo "\e[!] Trying to redeem Voucher : COBAINGOJEK !\n";
+            sleep(3);
+            goto ride;
+            }
+            ride:
+            $claim = ride($verif);
+            if ($claim == false ) {
+                echo "\e[!]".$claim['errors'][0]['message']."\n";
+                sleep(3);
+                echo "\e[!] Trying to redeem Voucher : AYOCOBAGOJEK !\n";
+                sleep(3);
+
+            }
+            else{
+                echo "\e[+] ".$claim."\n";
+                sleep(3);
+                echo "\e[!] Trying to redeem Voucher : AYOCOBAGOJEK !\n";
+                sleep(3);
+                goto pengen;
+            }
+            pengen:
+            $claim = cekvocer($verif);
+            if ($claim == false ) {
+                echo "\VOUCHER INVALID/GAGAL REDEEM\n";
+            }
+            else{
+                echo "\e[+] ".$claim."\n";
+                              
         }
-        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGORIDEPAY"}');
-        $message = fetch_value($code1,'"sukses bos":"','"');
-        if(strpos($code1, 'Promo kamu sudah bisa dipakai')){
-        echo "\n".color("green","+] Message: ".$message);
-        goto gofood;
-        }else{
-        echo "\n".color("green","-] Message: ".$message);
-        gofood:
-        echo "\n".color("yellow","!] Claim voc GOFOOD ");
-        echo "\n".color("yellow","!] sek entenono");
-        for($a=1;$a<=3;$a++){
-        echo color("yellow",".");
-        sleep(20);
-        }
-        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD090320A"}');
-        $message = fetch_value($code1,'"sukses bos":"','"');
-        if(strpos($code1, 'Promo kamu sudah bisa dipakai')){
-        echo "\n".color("green","+] Message: ".$message);
-        goto gofood;
-        }else{
-        echo "\n".color("green","-] Message: ".$message);
-        reff:
-        $data = '{"referral_code":"G-75SR565"}';    
-        $claim = request("/customer_referrals/v1/campaign/enrolment", $token, $data);
-        $message = fetch_value($claim,'"message":"','"');
-        if(strpos($claim, 'Promo kamu sudah bisa dipakai')){
-        echo "\n".color("green","+] Message: ".$message);
-        goto gofood;
-        }else{
-        echo "\n".color("green","-] Message: ".$message);
-        }
-        gofood:
-        echo "\n".color("yellow","!] Claim voc GOFOOD");
-        echo "\n".color("yellow","!] sek entenono");
-        for($a=1;$a<=3;$a++){
-        echo color("yellow",".");
-        sleep(3);
-        }
-        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"GOFOODSANTAI19"}');
-        $message = fetch_value($code1,'"sukses bos":"','"');
-        if(strpos($code1, 'Promo kamu sudah bisa dipakai')){
-        echo "\n".color("green","+] Message: ".$message);
-        goto gofood;
-        }else{
-        echo "\n".color("green","-] Message: ".$message);
-        gofood:
-        echo "\n".color("yellow","!] Claim voc GOFOOD ");
-        echo "\n".color("yellow","!] sek entenono");
-        for($a=1;$a<=3;$a++){
-        echo color("yellow",".");
-        sleep(20);
-        }
-        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"GOFOODSANTAI11"}');
-        $message = fetch_value($code1,'"sukses bos":"','"');
-        if(strpos($code1, 'Promo kamu sudah bisa dipakai')){
-        echo "\n".color("green","+] Message: ".$message);
-        goto gocar;
-        }else{
-        echo "\n".color("green","-] Message: ".$message);
-        gofood:
-        echo "\n".color("yellow","!] Claim voc GOFOOD ");
-        echo "\n".color("yellow","!] sek entenono");
-        for($a=1;$a<=3;$a++){
-        echo color("yellow",".");
-        sleep(3);
-        }
-        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOCAR14"}');
-        $message = fetch_value($code1,'"sukses bos":"','"');
-        if(strpos($code1, 'Promo kamu sudah bisa dipakai')){
-        echo "\n".color("green","+] Message: ".$message);
-        goto gofood;
-        }else{
-        echo "\n".color("green","-] Message: ".$message);
-        reff:
-        $data = '{"referral_code":"G-75SR565"}';    
-        $claim = request("/customer_referrals/v1/campaign/enrolment", $token, $data);
-        $message = fetch_value($claim,'"message":"','"');
-        if(strpos($claim, 'Promo kamu sudah bisa dipakai')){
-        echo "\n".color("green","+] Message: ".$message);
-        goto gofood;
-        }else{
-        echo "\n".color("green","-] Message: ".$message);
-        }
-        gofood:
-        echo "\n".color("yellow","!] Claim voc GOFOOD");
-        echo "\n".color("yellow","!] sek entenono");
-        for($a=1;$a<=3;$a++){
-        echo color("yellow",".");
-        sleep(3);
-        }
-        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"GOFOODSANTAI08"}');
-        $message = fetch_value($code1,'"sukses bos":"','"');
-        if(strpos($code1, 'Promo kamu sudah bisa dipakai')){
-        echo "\n".color("green","+] Message: ".$message);
-        sleep(1);
-        sleep(3);
-        $cekvoucher = request('/gopoints/v3/wallet/vouchers?limit=10&page=1', $token);
-        $total = fetch_value($cekvoucher,'"total_vouchers":',',');
-        $voucher3 = getStr1('"title":"','",',$cekvoucher,"3");
-        $voucher1 = getStr1('"title":"','",',$cekvoucher,"1");
-        $voucher2 = getStr1('"title":"','",',$cekvoucher,"2");
-        $voucher4 = getStr1('"title":"','",',$cekvoucher,"4");
-        $voucher5 = getStr1('"title":"','",',$cekvoucher,"5");
-        $voucher6 = getStr1('"title":"','",',$cekvoucher,"6");
-        echo "\n".color("yellow","!] Total voucher ".$total." : ");
-        echo color("green","1. ".$voucher1);
-        echo "\n".color("green","                     2. ".$voucher2);
-        echo "\n".color("green","                     3. ".$voucher3);
-        echo "\n".color("green","                     4. ".$voucher4);
-        echo "\n".color("green","                     5. ".$voucher5);
-        echo "\n".color("green","                     6. ".$voucher6);
-        $expired1 = getStr1('"expiry_date":"','"',$cekvoucher,'1');
-        $expired2 = getStr1('"expiry_date":"','"',$cekvoucher,'2');
-        $expired3 = getStr1('"expiry_date":"','"',$cekvoucher,'3');
-        $expired4 = getStr1('"expiry_date":"','"',$cekvoucher,'4');
-        $expired5 = getStr1('"expiry_date":"','"',$cekvoucher,'5');
-        $expired6 = getStr1('"expiry_date":"','"',$cekvoucher,'6');
-         setpin:
-         echo "\n".color("nevy","?] Mau set pin?: y/n ");
-         $pilih1 = trim(fgets(STDIN));
-         if($pilih1 == "y" || $pilih1 == "Y"){
-         //if($pilih1 == "y" && strpos($no, "628")){
-         echo color("red","========( PIN ANDA = 112233 )========")."\n";
-         $data2 = '{"pin":"112233"}';
-         $getotpsetpin = request("/wallet/pin", $token, $data2, null, null, $uuid);
-         echo "Otp set pin: ";
-         $otpsetpin = trim(fgets(STDIN));
-         $verifotpsetpin = request("/wallet/pin", $token, $data2, null, $otpsetpin, $uuid);
-         echo $verifotpsetpin;
-         }else if($pilih1 == "n" || $pilih1 == "N"){
-         die();
-         }else{
-         echo color("red","-] GAGAL!!!\n");
-         }
-         }
-         }
-         }
-         }else{
-         goto setpin;
-         }
-         }else{
-         echo color("red","-] Otp yang anda input salah");
-         echo"\n==================================\n\n";
-         echo color("yellow","!] Silahkan input kembali\n");
-         goto otp;
-         }
-         }else{
-         echo color("red","NOMOR SUDAH TERDAFTAR/SALAH !!!");
-         echo "\nMau ulang? (y/n): ";
-         $pilih = trim(fgets(STDIN));
-         if($pilih == "y" || $pilih == "Y"){
-         echo "\n==============Register==============\n";
-         goto ulang;
-         }else{
-         echo "\n==============Register==============\n";
-         goto ulang;
-  }
- }
-}
-echo change()."\n"; ?>
+    }
+    }
+    }
+
+
+?>  
